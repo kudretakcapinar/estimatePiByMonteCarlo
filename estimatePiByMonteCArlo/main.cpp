ad924156044fd5a850135431830956d9245dc57b
kudret akcapinar <kudretakcapinar@gmail.com>
2022-04-09 15:11:59 +0200

    Initial Commit

---
+++ estimatePiByMonteCArlo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  estimatePiByMonteCArlo
+//
+//  Created by kudret akcapinar on 9.04.2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
